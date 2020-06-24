1711098ad62d70a433f063affd1e03e95947d189
shanemoyo <smoyo@pdx.edu>
2020-06-24 16:18:17 -0700

    wip

--- src/components/gists/gist/index.js
+++ src/components/gists/gist/index.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react'; 
 import Resource from '../../utils/Resource';
 import github from '../../../services/api'; 
+import { useParams } from 'react-router-dom'; 
 
 
 
-export default class Gist extends Component { 
-    
-    render() { 
-        const id = this.props.match.params.id; 
-        return (
-            <Resource fetch={() => github.getGistByid(id)} render={({ resource: gist, loading, error }) => {
-                if(loading) {
-                    return <p>Loading...</p>;
-                } else if (error) {
-                    return <p style={{ fontSize: 12, color: 'red'}}>Something went wrong. Please try again later</p>;
-                } else {
-                    return (
-                        <section style={{ width: '80%', margin: 'auto'}}> 
-                            <h3>Gist: {gist.id}</h3>
-                        </section>
-                    );
-                } 
-            }}/>       
-        )      
-    }
+export default function Gist() { 
+     const { id } = useParams(); 
+    return (
+        <Resource fetch={() => github.getGistByid(id)} render={({ resource: gist, loading, error }) => {
+            if(loading) {
+                return <p>Loading...</p>;
+            } else if (error) {
+                return <p style={{ fontSize: 12, color: 'red'}}>Something went wrong. Please try again later</p>;
+            } else {
+                return (
+                    <section style={{ width: '80%', margin: 'auto'}}> 
+                        <h3>Gist: {gist.id}</h3>
+                    </section>
+                );
+            } 
+        }}/>       
+    )      
+ }
 
-}
+
 
 
